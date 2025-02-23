de8e35d5810c15396184241b7dec3019a3162a63
Lawrence <lawrence.ley@contextsolutions.ca>
2025-02-23 13:57:54 -0500

    - added mesh and jest testing tools - created alwaysSucceeds test suite - ported aiken faucet test suite - add typed validator for clarity

--- src/AlwaysSucceeds.hs
+++ src/AlwaysSucceeds.hs
@@ -13,14 +13,26 @@
 
 module AlwaysSucceeds where
 
-import PlutusTx.Prelude (BuiltinData, BuiltinUnit, check, ($))
+import PlutusTx.Prelude (Bool, Bool(..), BuiltinData, BuiltinUnit, check, Maybe(Just), ($))
+import PlutusLedgerApi.V3 qualified as V3
 import PlutusLedgerApi.Data.V3 qualified as V3Data
 import PlutusTx (CompiledCode, compile)
+
+typedValidator :: V3.Datum -> V3.Redeemer-> Bool
+typedValidator _datum _redeemer = True
 
 untypedValidator :: BuiltinData -> BuiltinUnit
 untypedValidator scriptContext =
   check
-    $ V3Data.unsafeFromBuiltinData scriptContext
+    $ case V3Data.unsafeFromBuiltinData scriptContext of
+      V3Data.ScriptContext
+        _txInfo
+        (V3Data.Redeemer redeemer)
+        (V3Data.SpendingScript _ (Just (V3Data.Datum datum))) ->
+          typedValidator
+            (V3.unsafeFromBuiltinData datum)
+            (V3.unsafeFromBuiltinData redeemer)
+      _ -> False
 
 {-# INLINEABLE untypedValidator #-}
 alwaysSucceedsScript :: CompiledCode (BuiltinData -> BuiltinUnit)
